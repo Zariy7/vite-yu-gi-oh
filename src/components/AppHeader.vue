572d86e34938425870fda7326831c7755498f591
Zariy7 <nazariy.hereha@gmail.com>
2024-01-11 08:41:17 +0100

    header working

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,16 @@ export default {
 </script>
 
 <template>
-    <div>
-        Header
+    <div class="d-flex">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg" class="logo">
+        <h1 class="align-self-center">
+            Yu-Gi-Oh Api
+        </h1>
     </div>
 </template>
 
 <style lang="scss">
-
+    .logo{
+        height: 100px;
+    }
 </style>
